1e62284c18eee54a8f9615ccbdd96e9ea348476d
Rishideep Singh Rathore <rishideeprathore@gmail.com>
2025-11-09 02:56:11 +0530

    added the add product, update product and delete product api's

--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -1,12 +1,22 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.ProductDTO;
 import com.ecommerce.project.payload.ProductResponse;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface ProductService {
 
-    ProductDTO addProduct(Long categoryId, Product product);
+    ProductDTO addProduct(Long categoryId, ProductDTO product);
 
     ProductResponse getAllProducts();
+
+    ProductResponse searchByCategory(Long categoryId);
+
+    ProductResponse searchProductByKeyword(String keyword);
+
+    ProductDTO updateProduct(Long productId, ProductDTO product);
+
+    ProductDTO deleteProduct(Long productId);
+
+    ProductDTO updateProductImage(Long productId, MultipartFile image);
 }
--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -10,9 +10,11 @@ import com.ecommerce.project.repositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService{
@@ -26,9 +28,10 @@ public class ProductServiceImpl implements ProductService{
     private ModelMapper modelMapper;
 
     @Override
-    public ProductDTO addProduct(Long categoryId, Product product) {
+    public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
         Category category=categoryRepository.findById(categoryId)
                 .orElseThrow(()->new ResourceNotFoundException("Category","categoryId",categoryId));
+        Product product=modelMapper.map(productDTO,Product.class);
         product.setImage("default.png");
         product.setCategory(category);
         double specialPrice= product.getPrice()-((product.getDiscount() * 0.01)* product.getPrice());
@@ -47,4 +50,98 @@ public class ProductServiceImpl implements ProductService{
         productResponse.setContent(productDTOS);
         return productResponse;
     }
+
+    @Override
+    public ProductResponse searchByCategory(Long categoryId) {
+        Category category=categoryRepository.findById(categoryId)
+                .orElseThrow(()->new ResourceNotFoundException("Category","categoryId",categoryId));
+        List<Product> products=productRepository.findByCategoryOrderByPriceAsc(category);
+        List<ProductDTO> productDTOS=products.stream()
+                .map(product -> modelMapper.map(product,ProductDTO.class))
+                .toList();
+        ProductResponse productResponse=new ProductResponse();
+        productResponse.setContent(productDTOS);
+        return productResponse;
+
+    }
+
+    @Override
+    public ProductResponse searchProductByKeyword(String keyword) {
+
+        List<Product> products=productRepository.findByProductNameLikeIgnoreCase('%'+keyword+'%');
+        List<ProductDTO> productDTOS=products.stream()
+                .map(product -> modelMapper.map(product,ProductDTO.class))
+                .toList();
+        ProductResponse productResponse=new ProductResponse();
+        productResponse.setContent(productDTOS);
+        return productResponse;
+
+    }
+
+    @Override
+    public ProductDTO updateProduct(Long productId, ProductDTO productDTO) {
+        //get the existing product from DB
+        Product productFromDb=productRepository.findById(productId)
+                .orElseThrow(()->new ResourceNotFoundException("Product","productId",productId));
+        Product product=modelMapper.map(productDTO,Product.class);
+        // update the product info with the one in request body
+        productFromDb.setProductName(product.getProductName());
+        productFromDb.setDescription(product.getDescription());
+        productFromDb.setQuantity(product.getQuantity());
+        productFromDb.setDiscount(product.getDiscount());
+        productFromDb.setPrice(product.getPrice());
+        productFromDb.setSpecialPrice(product.getSpecialPrice());
+
+        // save to database
+        Product savedProduct=productRepository.save(productFromDb);
+        return modelMapper.map(savedProduct,ProductDTO.class);
+
+    }
+
+    @Override
+    public ProductDTO deleteProduct(Long productId) {
+        Product product=productRepository.findById(productId)
+                .orElseThrow(()->new ResourceNotFoundException("Product","productId",productId));
+        productRepository.delete(product);
+
+        return modelMapper.map(product,ProductDTO.class);
+    }
+
+    @Override
+    public ProductDTO updateProductImage(Long productId, MultipartFile image) {
+        //get the product from the db
+        Product productFromDb=productRepository.findById(productId)
+                .orElseThrow(()->new ResourceNotFoundException("Product","productId",productId));
+
+        //upload the image to the server(/image)
+        //get the file name of uploaded image
+        String path="images/";
+        String fileName=uploadImage(path,image);
+
+
+        //uploading the new file name to the product
+        productFromDb.setImage(fileName);
+
+        //save updated product
+        Product updatedProduct=productRepository.save(productFromDb);
+
+        //return DTO after mapping product to DTO
+        return modelMapper.map(updatedProduct,ProductDTO.class);
+    }
+
+    private String uploadImage(String path, MultipartFile file) {
+       //file names of all original/current file
+        String originalFileName=file.getName();
+
+        //generate a unique file name
+        String randomId= UUID.randomUUID().toString();
+        String fileName=randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+        String filePath=path + File.pathSeparator+fileName;
+
+        //check if path exist and create
+
+        //upload to server
+        //returning file name
+    }
+
 }
