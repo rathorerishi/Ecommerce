690f9886985f8bbd6eec5cd921d5479084513dea
Rishideep Singh Rathore <rishideeprathore@gmail.com>
2025-11-12 18:29:56 +0530

    implemented pagination and sorting

--- src/main/java/com/ecommerce/project/payload/ProductResponse.java
+++ src/main/java/com/ecommerce/project/payload/ProductResponse.java
@@ -11,5 +11,11 @@ import java.util.List;
 @AllArgsConstructor
 public class ProductResponse {
     private List<ProductDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalPages;
+    private boolean lastPage;
+
 
 }
